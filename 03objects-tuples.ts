8004fbe1a0ba893b381d303771c1099a46e1c9d9
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-20 07:40:04 -0500

    Tuples

---
+++ 03objects-tuples.ts
@@ -0,0 +1,11 @@
+let ourTuple: [number, boolean, string];
+
+ourTuple = [5, false, 'Coding God was here'];
+
+console.log(ourTuple)
+console.log(typeof ourTuple)
+
+ourTuple.push('Something new');
+
+console.log(ourTuple)
+console.log(typeof ourTuple)
