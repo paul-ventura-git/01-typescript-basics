e3ec56ce3e42cb87135585a045f002a76d02081b
paul-ventura-git <j.paul.ventura@gmail.com>
2025-09-23 16:15:48 -0500

    First commit

--- 01types-any.ts
+++ 01types-any.ts
@@ -4,7 +4,7 @@ console.log(typeof obj);
 console.log(obj.x);
 
 // None of the following lines of code will throw compiler errors.
-// Using `any` disables all further type checking, and it is assumed
+// Using `type any` disables all further type checking, and it is assumed
 // you know the environment better than TypeScript.
 // obj.foo();
 // obj();
@@ -21,3 +21,16 @@ console.log(typeof obj);
 const n: number = obj;
 console.log(n);
 console.log(typeof n);
+
+/**
+ *  Instrucciones para compilar y ejecutar:
+ * 1. Asegurarse de tener instalado Node.js y TypeScript
+ *    npm install typescript --save-dev
+ * 2. Configurar el archivo tsconfig.json si es necesario
+ * 3. Abrir una terminal y navegar al directorio del archivo
+ * 4. Ejecutar el comando:
+ *    npx tsc 01types-any.ts --target es6 --module commonjs --strict --noEmit
+ * 5. Si no hay errores, ejecutar el archivo compilado con Node.js:
+ *    node 01types-any.js  
+ * 
+ */
